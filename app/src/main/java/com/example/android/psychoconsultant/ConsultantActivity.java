88c8ffa94c621717c9327713a3b5e6eec790332b
DoaaMostafa2011 <37475278+DoaaMostafa2011@users.noreply.github.com>
2019-05-30 23:10:55 +0200

    submit3

--- app/src/main/java/com/example/android/psychoconsultant/ConsultantActivity.java
+++ app/src/main/java/com/example/android/psychoconsultant/ConsultantActivity.java
@@ -29,6 +29,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
